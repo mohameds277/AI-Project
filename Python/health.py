19db1d0a3392b486e7ed75a51fa762b51822f261
mohamed <mohameds77@gmail.com>
2022-04-02 00:13:11 +0300

    first commit

---
+++ Python/health.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import re
+import psutil
+import shutil
+
+
+def check_disk_usage(disk):
+    du = shutil.disk_usage(disk)
+    free= du.free / du.total * 100
+    return free > 20 
+
+def check_cpu_usage():
+    usage  = psutil.cpu_percent(1)
+    return usage
+
+
+
+
+if not check_disk_usage("/") or not check_cpu_usage():
+    print("ERROR !!")
+else :
+    print("Everythin OK ")
